1846f6b8a4d543a6383c1ceda6ca808a847054db
DJW <dan@fake.com>
2018-08-27 23:25:23 +0100

    Minor tilesGetPrintString improvement

--- src/tiles.s
+++ src/tiles.s
@@ -5,11 +5,12 @@
 ;
 ; Current tiles are: 0 - water, 1 - grass, 2 - tree.
 const tileDataSize 16
+const tileDataShift 4
 db tileData 27,'[34;1;44m',226,150,147,0,0,0,	27,'[32;1;42m',226,150,145,0,0,0,	27,'[0;33;42m',240,159,140,178,0,0
 
 label tilesGetPrintString ; takes tile id in r0, returns addr in r0
-mov r1 tileDataSize
-mul r0 r0 r1
+mov r1 tileDataShift
+shl r0 r0 r1
 mov r1 tileData
 add r0 r0 r1
 ret
